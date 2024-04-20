5e899f7eeb9a7e9e52e4c9d67eee697fbfbdd7c5
Brijsunil2 <33044885+Brijsunil2@users.noreply.github.com>
2024-04-20 19:17:35 -0400

    Building

--- frontend/dist/assets/index.esm-LBj0RG9V.js
+++
@@ -1,1 +0,0 @@
-import{G as t}from"./index-pyFllL6d.js";function a(r){return t({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M85 277.375h259.704L225.002 397.077 256 427l171-171L256 85l-29.922 29.924 118.626 119.701H85v42.75z"}}]})(r)}export{a as I};
